edc931354dd09c71af4a0934578a86a14ad1e90e
Marcin Gapiński <49092713+margap1998@users.noreply.github.com>
2020-02-17 17:40:22 +0100

    sql update

--- do_mysql/taxi_ready_db.sql
+++ do_mysql/taxi_ready_db.sql
@@ -18,6 +18,8 @@ USE `PolTAXI` ;
 -- -----------------------------------------------------
 -- Table `PolTAXI`.`Pojazd`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`Pojazd` ;
+
 CREATE TABLE IF NOT EXISTS `PolTAXI`.`Pojazd` (
   `Numer_rejestracyjny` VARCHAR(10) NOT NULL,
   `Numer_boczny` INT(3) NOT NULL AUTO_INCREMENT,
@@ -34,6 +36,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `PolTAXI`.`Kierowca`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`Kierowca` ;
+
 CREATE TABLE IF NOT EXISTS `PolTAXI`.`Kierowca` (
   `PESEL` CHAR(11) NOT NULL,
   `Numer_rejestracyjny` VARCHAR(10) NOT NULL,
@@ -43,17 +47,20 @@ CREATE TABLE IF NOT EXISTS `PolTAXI`.`Kierowca` (
   PRIMARY KEY (`PESEL`, `Numer_rejestracyjny`),
   UNIQUE INDEX `PESEL_UNIQUE` (`PESEL` ASC) VISIBLE,
   INDEX `fk_Kierowca_Pojazd_idx` (`Numer_rejestracyjny` ASC) VISIBLE,
+  UNIQUE INDEX `Numer_rejestracyjny_UNIQUE` (`Numer_rejestracyjny` ASC) VISIBLE,
   CONSTRAINT `fk_Kierowca_Pojazd`
     FOREIGN KEY (`Numer_rejestracyjny`)
     REFERENCES `PolTAXI`.`Pojazd` (`Numer_rejestracyjny`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `PolTAXI`.`Ubezpieczenie`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`Ubezpieczenie` ;
+
 CREATE TABLE IF NOT EXISTS `PolTAXI`.`Ubezpieczenie` (
   `Numer_ubezpieczenia` VARCHAR(45) NOT NULL,
   `Numer_rejestracyjny` VARCHAR(10) NOT NULL,
@@ -63,7 +70,7 @@ CREATE TABLE IF NOT EXISTS `PolTAXI`.`Ubezpieczenie` (
   `OC` CHAR(1) NOT NULL DEFAULT 'T',
   `Data_zawarcia` DATE NOT NULL,
   `Data_zakonczenia` DATE NOT NULL,
-  `AC` CHAR(1) NOT NULL DEFAULT 'N',
+  `AC` CHAR(1) NULL DEFAULT 'N',
   `Dodatkowe_opcje` VARCHAR(160) NULL,
   PRIMARY KEY (`Numer_ubezpieczenia`, `Numer_rejestracyjny`),
   INDEX `fk_Ubezpieczenie_Pojazd1_idx` (`Numer_rejestracyjny` ASC) VISIBLE,
@@ -71,14 +78,16 @@ CREATE TABLE IF NOT EXISTS `PolTAXI`.`Ubezpieczenie` (
   CONSTRAINT `fk_Ubezpieczenie_Pojazd1`
     FOREIGN KEY (`Numer_rejestracyjny`)
     REFERENCES `PolTAXI`.`Pojazd` (`Numer_rejestracyjny`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `PolTAXI`.`Warsztat`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`Warsztat` ;
+
 CREATE TABLE IF NOT EXISTS `PolTAXI`.`Warsztat` (
   `Nazwa` VARCHAR(30) NOT NULL,
   `Adres` VARCHAR(45) NOT NULL,
@@ -89,6 +98,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `PolTAXI`.`Naprawa`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`Naprawa` ;
+
 CREATE TABLE IF NOT EXISTS `PolTAXI`.`Naprawa` (
   `Numer_faktury` VARCHAR(20) NOT NULL,
   `Data` DATE NOT NULL,
@@ -96,25 +107,27 @@ CREATE TABLE IF NOT EXISTS `PolTAXI`.`Naprawa` (
   `Numer_rejestracyjny` VARCHAR(10) NOT NULL,
   `Opis` VARCHAR(160) NOT NULL,
   `Koszt` DECIMAL(7,2) UNSIGNED NOT NULL DEFAULT 0,
-  PRIMARY KEY (`Numer_faktury`, `Nazwa`, `Numer_rejestracyjny`, `Data`),
   INDEX `fk_Naprawa_Warsztat1_idx` (`Nazwa` ASC) VISIBLE,
   INDEX `fk_Naprawa_Pojazd1_idx` (`Numer_rejestracyjny` ASC) VISIBLE,
+  PRIMARY KEY (`Numer_faktury`, `Data`, `Nazwa`, `Numer_rejestracyjny`),
   CONSTRAINT `fk_Naprawa_Warsztat1`
     FOREIGN KEY (`Nazwa`)
     REFERENCES `PolTAXI`.`Warsztat` (`Nazwa`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    ON UPDATE CASCADE,
   CONSTRAINT `fk_Naprawa_Pojazd1`
     FOREIGN KEY (`Numer_rejestracyjny`)
     REFERENCES `PolTAXI`.`Pojazd` (`Numer_rejestracyjny`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `PolTAXI`.`Rejon`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`Rejon` ;
+
 CREATE TABLE IF NOT EXISTS `PolTAXI`.`Rejon` (
   `Nazwa` VARCHAR(25) NOT NULL,
   PRIMARY KEY (`Nazwa`))
@@ -124,28 +137,32 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `PolTAXI`.`Obsluga`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`Obsluga` ;
+
 CREATE TABLE IF NOT EXISTS `PolTAXI`.`Obsluga` (
   `Kierowca` CHAR(11) NOT NULL,
   `Rejon` VARCHAR(25) NOT NULL,
   PRIMARY KEY (`Kierowca`, `Rejon`),
-  INDEX `fk_Kierowca_has_Rejon_Rejon1_idx` (`Rejon` ASC) VISIBLE,
-  INDEX `fk_Kierowca_has_Rejon_Kierowca1_idx` (`Kierowca` ASC) VISIBLE,
-  CONSTRAINT `fk_Kierowca_has_Rejon_Kierowca1`
+  INDEX `fk_Obsluga1_idx` (`Rejon` ASC) VISIBLE,
+  INDEX `fk_Obsluga2_idx` (`Kierowca` ASC) VISIBLE,
+  CONSTRAINT `fk_Obsluga_Kierowca1`
     FOREIGN KEY (`Kierowca`)
     REFERENCES `PolTAXI`.`Kierowca` (`PESEL`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Kierowca_has_Rejon_Rejon1`
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_Obsluga_Rejon1`
     FOREIGN KEY (`Rejon`)
     REFERENCES `PolTAXI`.`Rejon` (`Nazwa`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `PolTAXI`.`Ulica`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`Ulica` ;
+
 CREATE TABLE IF NOT EXISTS `PolTAXI`.`Ulica` (
   `Nazwa` VARCHAR(25) NOT NULL,
   `Rejon` VARCHAR(25) NOT NULL,
@@ -154,27 +171,32 @@ CREATE TABLE IF NOT EXISTS `PolTAXI`.`Ulica` (
   CONSTRAINT `fk_Ulica_Rejon1`
     FOREIGN KEY (`Rejon`)
     REFERENCES `PolTAXI`.`Rejon` (`Nazwa`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `PolTAXI`.`Klient`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`Klient` ;
+
 CREATE TABLE IF NOT EXISTS `PolTAXI`.`Klient` (
   `Numer_telefonu` VARCHAR(15) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `Data_rejestracji` DATE NOT NULL,
   `Imie` VARCHAR(20) NULL,
   `Nazwisko` VARCHAR(30) NULL,
-  PRIMARY KEY (`Numer_telefonu`))
+  PRIMARY KEY (`Numer_telefonu`),
+  UNIQUE INDEX `Numer_telefonu_UNIQUE` (`Numer_telefonu` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `PolTAXI`.`Stawka`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`Stawka` ;
+
 CREATE TABLE IF NOT EXISTS `PolTAXI`.`Stawka` (
   `Kod` INT(3) NOT NULL,
   `Opis` VARCHAR(100) NOT NULL,
@@ -188,9 +210,11 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `PolTAXI`.`Przejazd`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`Przejazd` ;
+
 CREATE TABLE IF NOT EXISTS `PolTAXI`.`Przejazd` (
-  `Nr_paragonu` VARCHAR(25) NOT NULL,
-  `PESEL` CHAR(11) NOT NULL,
+  `Numer_paragonu` VARCHAR(25) NOT NULL,
+  `PESEL` CHAR(11) NULL,
   `Numer_telefonu` VARCHAR(15) NOT NULL,
   `Stawka_Kod` INT(3) NOT NULL,
   `Data_rozpoczecia` TIMESTAMP NOT NULL,
@@ -202,29 +226,32 @@ CREATE TABLE IF NOT EXISTS `PolTAXI`.`Przejazd` (
   INDEX `fk_Przejazd_idx` (`Numer_telefonu` ASC) INVISIBLE,
   INDEX `fk_Przejazd_Stawka1_idx` (`Stawka_Kod` ASC) VISIBLE,
   INDEX `fk_Przejazd_Kierowca1_idx` (`PESEL` ASC) VISIBLE,
-  PRIMARY KEY (`Nr_paragonu`),
+  PRIMARY KEY (`Numer_paragonu`),
   UNIQUE INDEX `dane_unikalne_przejazdu` (`PESEL` ASC, `Numer_telefonu` ASC, `Data_rozpoczecia` ASC) VISIBLE,
+  UNIQUE INDEX `Numer_paragonu_UNIQUE` (`Numer_paragonu` ASC) VISIBLE,
   CONSTRAINT `fk_Przejazd_Klient1`
     FOREIGN KEY (`Numer_telefonu`)
     REFERENCES `PolTAXI`.`Klient` (`Numer_telefonu`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    ON UPDATE CASCADE,
   CONSTRAINT `fk_Przejazd_Stawka1`
     FOREIGN KEY (`Stawka_Kod`)
     REFERENCES `PolTAXI`.`Stawka` (`Kod`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    ON UPDATE CASCADE,
   CONSTRAINT `fk_Przejazd_Kierowca1`
     FOREIGN KEY (`PESEL`)
     REFERENCES `PolTAXI`.`Kierowca` (`PESEL`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `PolTAXI`.`Promocja`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`Promocja` ;
+
 CREATE TABLE IF NOT EXISTS `PolTAXI`.`Promocja` (
   `Kod` VARCHAR(10) NOT NULL,
   `Nazwa` VARCHAR(45) NOT NULL,
@@ -238,31 +265,33 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `PolTAXI`.`Aktywowana_Promocja`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`Aktywowana_Promocja` ;
+
 CREATE TABLE IF NOT EXISTS `PolTAXI`.`Aktywowana_Promocja` (
   `Kod` VARCHAR(10) NOT NULL,
   `Numer_telefonu` VARCHAR(15) NOT NULL,
   `Data_aktywacji` DATE NOT NULL,
   `Wykorzystanie` INT(1) UNSIGNED NOT NULL,
-  `Przejazd_Nr_paragonu` VARCHAR(25) NULL,
+  `Numer_paragonu` VARCHAR(25) NULL,
   PRIMARY KEY (`Kod`, `Numer_telefonu`, `Data_aktywacji`),
-  INDEX `fk_Promocja_has_Klient_Klient1_idx` (`Numer_telefonu` ASC) VISIBLE,
-  INDEX `fk_Promocja_has_Klient_Promocja1_idx` (`Kod` ASC) VISIBLE,
-  INDEX `fk_Aktywowana_Promocja_Przejazd1_idx` (`Przejazd_Nr_paragonu` ASC) VISIBLE,
-  CONSTRAINT `fk_Promocja_has_Klient_Promocja1`
+  INDEX `fk_Aktywowana_Promocja1_idx` (`Numer_telefonu` ASC) VISIBLE,
+  INDEX `fk_Aktywowana_Promocja2_idx` (`Kod` ASC) VISIBLE,
+  INDEX `fk_Aktywowana_Promocja_Przejazd1_idx` (`Numer_paragonu` ASC) VISIBLE,
+  CONSTRAINT `fk_AktywowanaPromocja_Promocja1`
     FOREIGN KEY (`Kod`)
     REFERENCES `PolTAXI`.`Promocja` (`Kod`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Promocja_has_Klient_Klient1`
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_AktywowanaPromocja_Klient1`
     FOREIGN KEY (`Numer_telefonu`)
     REFERENCES `PolTAXI`.`Klient` (`Numer_telefonu`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    ON UPDATE CASCADE,
   CONSTRAINT `fk_Aktywowana_Promocja_Przejazd1`
-    FOREIGN KEY (`Przejazd_Nr_paragonu`)
-    REFERENCES `PolTAXI`.`Przejazd` (`Nr_paragonu`)
+    FOREIGN KEY (`Numer_paragonu`)
+    REFERENCES `PolTAXI`.`Przejazd` (`Numer_paragonu`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 USE `PolTAXI` ;
@@ -275,18 +304,31 @@ CREATE TABLE IF NOT EXISTS `PolTAXI`.`Pojazdy_z_ubezpieczeniem` (`Numer_ubezpiec
 -- -----------------------------------------------------
 -- Placeholder table for view `PolTAXI`.`KierowcyPojazdy`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `PolTAXI`.`KierowcyPojazdy` (`Numer_rejestracyjny` INT, `Numer_boczny` INT, `Marka` INT, `Model` INT, `Rocznik` INT, `Koniec_przegladu` INT, `Osob` INT, `PESEL` INT, `Imie` INT, `Nazwisko` INT, `Data_zatrudnienia` INT);
+CREATE TABLE IF NOT EXISTS `PolTAXI`.`KierowcyPojazdy` (`PESEL` INT, `Imie` INT, `Nazwisko` INT, `Data_zatrudnienia` INT, `Numer_rejestracyjny` INT, `Numer_boczny` INT, `Marka` INT, `Model` INT, `Rocznik` INT, `Koniec_przegladu` INT, `Osob` INT);
+
+-- -----------------------------------------------------
+-- Placeholder table for view `PolTAXI`.`naprawaWarsztat`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PolTAXI`.`naprawaWarsztat` (`Numer_faktury` INT, `Data` INT, `Nazwa` INT, `Adres` INT, `Numer_rejestracyjny` INT, `Opis` INT, `Koszt` INT);
+
+-- -----------------------------------------------------
+-- Placeholder table for view `PolTAXI`.`naprawaPojazd`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PolTAXI`.`naprawaPojazd` (`id` INT);
 
 -- -----------------------------------------------------
 -- procedure Dodaj_klienta
 -- -----------------------------------------------------
 
+USE `PolTAXI`;
+DROP procedure IF EXISTS `PolTAXI`.`Dodaj_klienta`;
+
 DELIMITER $$
 USE `PolTAXI`$$
-CREATE PROCEDURE Dodaj_klienta (nr VARCHAR(15), e VARCHAR(45), rej DATE)
+CREATE PROCEDURE Dodaj_klienta (nr VARCHAR(15), em VARCHAR(45), rej DATE, im VARCHAR(20), naz varchar(30))
 BEGIN
-	INSERT INTO Klient(Numer_telefonu, email, Data_rejestracji)
-		values(nr,em,rej);
+	INSERT INTO Klient(Numer_telefonu, email, Data_rejestracji,Imie, Nazwisko)
+		values(nr,em,rej,im,naz);
 END$$
 
 DELIMITER ;
@@ -294,6 +336,9 @@ DELIMITER ;
 -- -----------------------------------------------------
 -- function Najstarszy
 -- -----------------------------------------------------
+
+USE `PolTAXI`;
+DROP function IF EXISTS `PolTAXI`.`Najstarszy`;
 
 DELIMITER $$
 USE `PolTAXI`$$
@@ -313,16 +358,18 @@ DELIMITER ;
 -- function Placa
 -- -----------------------------------------------------
 
+USE `PolTAXI`;
+DROP function IF EXISTS `PolTAXI`.`Placa`;
+
 DELIMITER $$
 USE `PolTAXI`$$
-CREATE FUNCTION `Placa` (vID int(3),vod date, vdo date)
+CREATE FUNCTION `Placa` (vID CHAR(11),vod date, vdo date, vplaca int(5))
 RETURNS DECIMAL deterministic
 BEGIN
 	DECLARE vPlaca,vZarobek,vPodstawa DECIMAL;
 	DECLARE vPrzejazdow INT;
-    SELECT Placa_podstawowa INTO vPodstawa FROM Kierowca WHERE idKierowcy=vID;
     SELECT COUNT(*),SUM(Cena) INTO vPrzejazdow,vZarobek FROM Przejazd 
-		WHERE idKierowcy = vID AND Data_rozpoczecia <=vdo AND Data_rozpoczecia>= vod; 
+		WHERE PESEL = vID AND Data_rozpoczecia <=vdo AND Data_rozpoczecia>= vod; 
 	CASE
         WHEN vPrzejazdow>=640 THEN SET vPlaca = vPodstawa + vZarobek*0.10;
 		WHEN vPrzejazdow<200 THEN SET vPlaca = vPodstawa;
@@ -336,6 +383,9 @@ DELIMITER ;
 -- -----------------------------------------------------
 -- procedure Dodaj_kierowce
 -- -----------------------------------------------------
+
+USE `PolTAXI`;
+DROP procedure IF EXISTS `PolTAXI`.`Dodaj_kierowce`;
 
 DELIMITER $$
 USE `PolTAXI`$$
@@ -354,6 +404,9 @@ DELIMITER ;
 -- procedure Dodaj_pojazd
 -- -----------------------------------------------------
 
+USE `PolTAXI`;
+DROP procedure IF EXISTS `PolTAXI`.`Dodaj_pojazd`;
+
 DELIMITER $$
 USE `PolTAXI`$$
 CREATE PROCEDURE `Dodaj_pojazd` (nr_rej varchar(10), mar varchar(15), modelpoj varchar(45), rocznik year, przeg date, rej_osob int(1))
@@ -365,9 +418,32 @@ END;$$
 DELIMITER ;
 
 -- -----------------------------------------------------
+-- function przejazdy
+-- -----------------------------------------------------
+
+USE `PolTAXI`;
+DROP function IF EXISTS `PolTAXI`.`przejazdy`;
+
+DELIMITER $$
+USE `PolTAXI`$$
+CREATE FUNCTION `przejazdy` (pes CHAR(11))
+returns DECIMAL(4) deterministic
+BEGIN
+DECLARE wynik DECIMAL(4);
+SELECT COUNT(*) INTO wynik FROM przejazdy WHERE PESEL=pes 
+	AND Data_rozpoczecia<=current_date() 
+    AND Data_rozpoczecia>= date_sub(current_date(), INTERVAL 30 DAY)
+    GROUP BY PESEL;
+return wynik;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
 -- View `PolTAXI`.`Pojazdy_z_ubezpieczeniem`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `PolTAXI`.`Pojazdy_z_ubezpieczeniem`;
+DROP VIEW IF EXISTS `PolTAXI`.`Pojazdy_z_ubezpieczeniem` ;
 USE `PolTAXI`;
 CREATE  OR REPLACE VIEW Pojazdy_z_ubezpieczeniem AS
 	SELECT * FROM Pojazd NATURAL JOIN Ubezpieczenie;
@@ -376,9 +452,28 @@ CREATE  OR REPLACE VIEW Pojazdy_z_ubezpieczeniem AS
 -- View `PolTAXI`.`KierowcyPojazdy`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `PolTAXI`.`KierowcyPojazdy`;
+DROP VIEW IF EXISTS `PolTAXI`.`KierowcyPojazdy` ;
 USE `PolTAXI`;
 CREATE  OR REPLACE VIEW `KierowcyPojazdy` AS
-	SELECT * FROM Kierowca natural join Pojazd;
+	SELECT k.PESEL, k.Imie, k.Nazwisko, k.Data_zatrudnienia, p.Numer_rejestracyjny, p.Numer_boczny, p.Marka, p.Model, p.Rocznik, p.Koniec_przegladu, p.Osob FROM Kierowca k join Pojazd p ON k.Numer_rejestracyjny = p.Numer_rejestracyjny;
+
+-- -----------------------------------------------------
+-- View `PolTAXI`.`naprawaWarsztat`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`naprawaWarsztat`;
+DROP VIEW IF EXISTS `PolTAXI`.`naprawaWarsztat` ;
+USE `PolTAXI`;
+CREATE OR REPLACE VIEW `naprawaWarsztat` AS
+	SELECT N.Numer_faktury, N.`Data`, N.Nazwa, W.Adres, N.Numer_rejestracyjny, N.Opis, N.Koszt FROM naprawa N Join warsztat W ON N.Nazwa = W.Nazwa;
+
+-- -----------------------------------------------------
+-- View `PolTAXI`.`naprawaPojazd`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `PolTAXI`.`naprawaPojazd`;
+DROP VIEW IF EXISTS `PolTAXI`.`naprawaPojazd` ;
+USE `PolTAXI`;
+CREATE  OR REPLACE VIEW `naprawaPojazd` AS
+	SELECT * FROM pojazd NATURAL JOIN naprawa;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
